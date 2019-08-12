70514cd44149c1787f16ad5e8f78080d2a3345ed
AndreyDodonov <Nordhummer@gmail.com>
2019-08-12 11:01:12 +0300

    moreBtn init

---
+++ src/modules/moreBtn.js
@@ -0,0 +1,16 @@
+const moreBtn = () => {
+    const moreBtn = document.querySelector('.add-sentence-btn'),
+          hiddenCards = document.querySelector('.hidden'), 
+          hiddenCard = document.querySelector('.visible-sm-block');
+
+    moreBtn.addEventListener('click', () => {
+       
+        hiddenCards.style.display = 'flex';
+      
+        hiddenCard.style.display = 'flex';
+        moreBtn.style.display = 'none';
+   });
+
+};
+
+export default moreBtn;
