8118804a4a31c7e29db6d404cce76a1028e24af1
AndreyDodonov <Nordhummer@gmail.com>
2019-08-15 16:51:51 +0300

    babel + configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/* polyfills */
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'fetch-polyfill';
+import  'es6-promise';
+import 'formdata-polyfill';
+
+/* modules */
+
 import popup from './modules/popup';
 import sendForm from './modules/sendForm';
 import getValidUserData from './modules/validation';
